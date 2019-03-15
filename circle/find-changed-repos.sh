0b125d9ac0e05d1e47b262f9355f8dadd812b287
Eric Wyler <ewyler@gmail.com>
2019-03-15 15:21:11 -0400

    make script actually work

--- circle/find-changed-repos.sh
+++ circle/find-changed-repos.sh
@@ -1,14 +1,11 @@
-DEPLOYABLE_PATHS=(
-  workspace-a
-  workspace-b
-  workspace-c
-)
+DEPLOYABLE_PATHS=( workspace-a workspace-b workspace-c )
 
 # Relevant paths to deploy
 to_deploy=()
+changed_files=`git diff --name-only HEAD~20 HEAD`
 
-for changed_file in `git diff --name-only HEAD~20 HEAD`; do
-  for path in $DEPLOYABLE_PATHS; do
+for changed_file in ${changed_files[@]}; do
+  for path in ${DEPLOYABLE_PATHS[@]}; do
     if [[ $changed_file == $path* ]];
     then
       to_deploy+=( $path )
@@ -20,12 +17,11 @@ done
 # De-dupe
 unique_deploy_paths=`printf "%s\n" "${to_deploy[@]}" | sort -u`
 
-echo $unique_deploy_paths
-
 git_root=`git rev-parse --show-toplevel`
 
 # Deploy
-for deploy_path in $unique_deploy_paths; do
+for deploy_path in ${unique_deploy_paths[@]}; do
   cd $git_root/$deploy_path
+  echo `pwd`
   echo "yarn deploy"
 done
