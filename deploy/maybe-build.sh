d2b9a5ee04a75a0c4453c564a11523b2fca0d0b8
Eric Wyler <ewyler@gmail.com>
2019-03-19 16:32:41 -0400

    jklfjldk

--- deploy/maybe-build.sh
+++ deploy/maybe-build.sh
@@ -1,13 +1,8 @@
 DIRECTORY_TO_BUILD=$1
 
-LAST_MERGE_COMMIT=$(git log --merges --pretty=format:’%h’ -n1 HEAD~1)
-
-if [ -z $LAST_MERGE_COMMIT ]; then
-  LAST_MERGE_COMMIT=HEAD^1
-  echo "Could not find last merge commit - assuming HEAD^1"
-else
-  echo "Last merge commit: $LAST_MERGE_COMMIT"
-fi
+# Assume master for now
+LAST_MERGE_COMMIT=master
+echo "Last merge commit: $LAST_MERGE_COMMIT"
 
 COMMITS_IN_DIR=`git log --oneline $LAST_MERGE_COMMIT..HEAD -- $DIRECTORY_TO_BUILD | wc -l`
 
