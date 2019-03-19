fdbe682b18190af1ca4690d9967923c00a9149b1
Eric Wyler <ewyler@gmail.com>
2019-03-19 15:34:41 -0400

    jklfd

--- deploy/maybe-build.sh
+++ deploy/maybe-build.sh
@@ -1,3 +1,5 @@
+set -o xtrace
+
 DIRECTORY_TO_BUILD=$1
 
 COMMIT_RANGE=$(echo $CIRCLE_COMPARE_URL | sed 's:^.*/compare/::g')
@@ -5,5 +7,7 @@ COMMIT_RANGE=$(echo $CIRCLE_COMPARE_URL | sed 's:^.*/compare/::g')
 echo $CIRCLE_COMPARE_URL
 echo $COMMIT_RANGE
 echo $DIRECTORY_TO_BUILD
+
+env
 
 git log --oneline $COMMIT_RANGE -- $DIRECTORY_TO_BUILD
