7170a426d6a91fc5562397fa2b618743d20b9f6f
Eric Wyler <ewyler@gmail.com>
2019-03-19 17:02:03 -0400

    jklfd

--- deploy/maybe-build.sh
+++ deploy/maybe-build.sh
@@ -2,6 +2,10 @@ set -o xtrace
 
 DIRECTORY_TO_BUILD=$1
 
+# Will be needed if we allow merge commits rather than requiring squashes on head - but we can probably
+# just always require squashes for now
+# git diff $(git log --merges --pretty=format:’%h’ -n1 HEAD~1)..HEAD
+
 # Don't use master, circleCI does some weird stuff and moves it ahead of actual master
 MASTER_HASH=`git rev-parse origin/master`
 HEAD_HASH=`git rev-parse HEAD`
